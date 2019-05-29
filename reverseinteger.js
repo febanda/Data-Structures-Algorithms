08d06c57abf04b03a731639c257a4b278b9565f1
febanda01 <febanda91@gmail.com>
2019-05-28 20:58:15 -0500

    solution-for-reverseint

--- reverseinteger.js
+++ reverseinteger.js
@@ -4,3 +4,15 @@
 // reverseInt(15) === 51
 // reverseInt(981) === 189
 // reverseInt(-15) === -51
+
+function reverseInt(n){
+    if(Math.sign(n) === -1){
+        const stringNumber = n.toString().split('').reverse().join('')
+        console.log(-parseInt(stringNumber))
+    }else if(Math.sign(n) === 1){
+        const stringNumber = n.toString().split('').reverse().join('')
+        console.log(parseInt(stringNumber))
+    }else{
+        console.log(n)
+    }
+}
