d3d6fd549097747e8fae173c0ff5b7300f76a2f7
febanda01 <febanda91@gmail.com>
2019-06-03 19:30:36 -0500

    intructions-added

--- maxChar.js
+++ maxChar.js
@@ -1,3 +1,14 @@
-function fizzBuzz(){
-    
+//---Directions 
+
+//Given a string, return the character that is most commenly used in the string 
+
+//--Examples 
+// maxChar('abcccccccd')=== 'c'
+
+function maxChar(str){
+
+
+
 }
+
+
