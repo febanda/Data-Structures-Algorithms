202286683dac6fb11dd24792ab6a997be15d3106
febanda01 <febanda91@gmail.com>
2019-06-11 20:17:06 -0500

    tic-tac-toe-solution

--- arrayChunk.js
+++ arrayChunk.js
@@ -19,3 +19,24 @@ function chunk(array, size){
     return chunked 
 }
 
+
+
+function tictactoe(arr){
+    for(let i = arr.length - 1; i>0; i--){
+        let j = Math.floor(Math.random() * (i+1)); //random index from 0 to i 
+        [arr[i], arr[j]] = [arr[j], arr[i]] // swap elements 
+    }
+
+
+    for(var i = 0; i<arr.length; i++){
+        if(arr[i] === arr[i + 1] && arr[i + 1] === arr[i + 2]){
+            return true 
+        }else if(arr[i+1] === arr[i + 2] && arr[i + 2] === arr[i + 3]) {
+            return true 
+        }else if(arr[i+2] === arr[i + 3] && arr[i + 3] === arr[i + 4]){
+            return true
+        }else{
+            return false 
+        }
+    }
+}
