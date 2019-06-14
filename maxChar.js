01787e87193eecfa5815e26dd6ac4049967725b5
febanda01 <febanda91@gmail.com>
2019-06-13 19:17:20 -0500

    update

--- maxChar.js
+++ maxChar.js
@@ -32,25 +32,29 @@ function maxChar(str){
 
 
 
-function maxChar(arr){
-    const chars = {}
-    const count = 1
+// function maxChar(str){
+//     const chars = {}
+//     let max = 0
+//     let maxChar = ''
 
-    for(let char of arr){
-        if(!chars[char]){
-            chars[char] = 1
-        }else{
-            chars[char]++
-        }
-    }
+//     for(let char of str){
+//         if(!chars[char]){
+//             chars[char] = 1
+//         }else{
+//             chars[char]++
+//         }
+//     }
 
-    for(let char in chars){
-        if(chars[char] === count){
-            return chars[char]
-        }
-    }
+//     for(let char in chars){
+//         if(chars[char] > max){
+//             max = chars[char]
+//             maxChar = char 
+//         }
+//     }
+
+//     return maxChar 
 
 
-}
+// }
 
 
