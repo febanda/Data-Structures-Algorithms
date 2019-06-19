3cb1d97e27475eaa6a6de6558cf9b6d615a97ef4
febanda01 <febanda91@gmail.com>
2019-06-19 18:49:09 -0500

    second-solution-added

--- arrayChunk.js
+++ arrayChunk.js
@@ -6,37 +6,31 @@
 //chunk([1,2,3,4,5], 2) --> [[1,2], [3,4], [5]]
 
 
+//solution 1 
+// function chunk(array, size){
+//     const chunked = []
+//     for(let element of array){
+//         const lastElement = chunked[chunked.length - 1]
+//         if(!lastElement || lastElement.length === size){
+//             chunked.push([element])
+//         }else{
+//             lastElement.push(element)
+//         }
+//     }
+//     return chunked 
+// }
+
+
+
+//solution 2 
 function chunk(array, size){
     const chunked = []
-    for(let element of array){
-        const lastElement = chunked[chunked.length - 1]
-        if(!lastElement || lastElement.length === size){
-            chunked.push([element])
-        }else{
-            lastElement.push(element)
-        }
+    let index = 0 
+    while(index < array.length){
+        chunked.push(array.slice(index, index + size))
+        index += size
     }
     return chunked 
+
 }
 
-
-
-function tictactoe(arr){
-    for(let i = arr.length - 1; i>0; i--){
-        let j = Math.floor(Math.random() * (i+1)); //random index from 0 to i 
-        [arr[i], arr[j]] = [arr[j], arr[i]] // swap elements 
-    }
-
-
-    for(var i = 0; i<arr.length; i++){
-        if(arr[i] === arr[i + 1] && arr[i + 1] === arr[i + 2]){
-            return true 
-        }else if(arr[i+1] === arr[i + 2] && arr[i + 2] === arr[i + 3]) {
-            return true 
-        }else if(arr[i+2] === arr[i + 3] && arr[i + 3] === arr[i + 4]){
-            return true
-        }else{
-            return false 
-        }
-    }
-}
