0ef0f784f18344ae8dd65a99961b9beba42d89d3
febanda01 <febanda91@gmail.com>
2019-07-22 16:02:34 -0500

    solution-added

--- printing-steps.js
+++ printing-steps.js
@@ -1,5 +1,6 @@
 
 
+//Iterative Solution
 function printSteps(n){
     for(let row = 0; row < n; row++){
         let stair = ''
@@ -16,49 +17,13 @@ function printSteps(n){
 }
 
 
-
-
-function printSteps(n){
-
-    for(let c =0; c<n; c++){
-        let stairs = ''
-        for(let r=0; r<n; r++){
-            if(c <= r){
-                stairs += '#'
-            }else{
-                stairs += ' '
-            }
-        }
-        console.log(stairs)
-    }
-
-
-}
-
-
-
-function capitalize(str){
-
-    const words = []
-
-    for(let word of str.split(' ')){
-        words.push(word[0] + word.slice(1))
-    }
-    return words.join(' ')
-}
+//Recursive Solution 
 
 
 
 
-function capitalize(str){
-
-    let results = ''
-
-    
 
 
-
-}
 
 
 
